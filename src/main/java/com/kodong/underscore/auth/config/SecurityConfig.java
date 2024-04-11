e29c61501731a043db04bc68d868b0e20a55aca3
ko <iamkokoko123@gmail.com>
2024-04-11 15:33:27 +0900

    Feat: [OAuth] Security 설정

---
+++ src/main/java/com/kodong/underscore/auth/config/SecurityConfig.java
@@ -0,0 +1,78 @@
+package com.kodong.underscore.auth.config;
+
+import com.kodong.underscore.auth.jwt.CustomLogoutFilter;
+import com.kodong.underscore.auth.jwt.JWTFilter;
+import com.kodong.underscore.auth.jwt.JWTUtil;
+import com.kodong.underscore.auth.oauth2.CustomSuccessHandler;
+import com.kodong.underscore.auth.service.CustomOAuth2UserService;
+import com.kodong.underscore.auth.service.RefreshTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final RefreshTokenService refreshTokenService;
+    private final CustomSuccessHandler customSuccessHandler;
+    private final JWTUtil jwtUtil;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        //csrf disable
+        http
+                .csrf((auth) -> auth.disable());
+
+        //From 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+
+        //HTTP Basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        // JWT 필터 추가
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+
+        // 로그아웃 필터
+        http
+                .addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshTokenService), LogoutFilter.class);
+
+
+        //oauth2
+        http
+                .oauth2Login((oauth2) -> oauth2
+                        .loginPage("/auth/join")
+                        .userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService)))
+                        .successHandler(customSuccessHandler)
+                );
+
+
+        //경로별 인가 작업
+        // todo 페이지별 인가 설정
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/**", "/reissue").permitAll());
+
+
+        //세션 설정 : STATELESS
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+
+}
