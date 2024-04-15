d4f409e8140a1a4ead099bf37318dd57c64e5caf
jjw0071 <jjw0071@naver.com>
2024-04-15 18:28:29 +0900

    feat : 상권변화 지표 repository에 필요한 메서드 추가
    Issues : #20

--- src/main/java/com/kodong/underscore/map/repository/IndexQuarterlyQuotientRepository.java
+++ src/main/java/com/kodong/underscore/map/repository/IndexQuarterlyQuotientRepository.java
@@ -1,9 +1,16 @@
 package com.kodong.underscore.map.repository;
 
+import com.kodong.underscore.map.entity.AdministrativeDistrict;
 import com.kodong.underscore.map.entity.IndexQuarterlyQuotient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IndexQuarterlyQuotientRepository extends JpaRepository<IndexQuarterlyQuotient,Long> {
+    Page<IndexQuarterlyQuotient> findAll(Pageable pageable);
+    Optional<IndexQuarterlyQuotient> findByStandardYearQuarterCodeAndAdministrativeDistrict(String standardYearQuarterCode, AdministrativeDistrict district);
 }
