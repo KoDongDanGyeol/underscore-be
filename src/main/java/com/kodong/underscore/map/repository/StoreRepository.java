c397b52de2f08eed9ae59c86da37f4b429264d85
jjw0071 <jjw0071@naver.com>
2024-04-15 18:29:04 +0900

    feat : 점포 repository에 필요한 메서드 추가
    Issues : #20

--- src/main/java/com/kodong/underscore/map/repository/StoreRepository.java
+++ src/main/java/com/kodong/underscore/map/repository/StoreRepository.java
@@ -1,9 +1,23 @@
 package com.kodong.underscore.map.repository;
 
+import com.kodong.underscore.map.entity.AdministrativeDistrict;
+import com.kodong.underscore.map.entity.Selling;
+import com.kodong.underscore.map.entity.ServiceIndustry;
 import com.kodong.underscore.map.entity.Store;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface StoreRepository extends JpaRepository<Store,Long> {
+    List<Store> findAllByServiceIndustryAndStandardYearQuarterCodeOrderBySimilarIndustryStoreCount(ServiceIndustry serviceIndustry, String standardYearQuarterCode);
+    Page<Store> findAll(Pageable pageable);
+
+    Optional<Store> findByStandardYearQuarterCodeAndAdministrativeDistrictAndServiceIndustry(
+            String standardYearQuarterCode, AdministrativeDistrict administrativeDistrict, ServiceIndustry serviceIndustry
+    );
 }
