37c7ca3468eaa7039531503b632c9fe2c91be17f
jjw0071 <jjw0071@naver.com>
2024-04-16 22:55:22 +0900

    feat : 법정동 코드를 찾아 행정동 코드 넘겨주는 함수 추가
    Issues : #29

--- src/main/java/com/kodong/underscore/map/repository/LegalDistrictRepository.java
+++ src/main/java/com/kodong/underscore/map/repository/LegalDistrictRepository.java
@@ -2,9 +2,16 @@ package com.kodong.underscore.map.repository;
 
 import com.kodong.underscore.map.entity.LegalDistrict;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface LegalDistrictRepository extends JpaRepository<LegalDistrict, Long> {
     Optional<LegalDistrict> findByLegalDistrictCode(String legalDistrictCode);
+
+    // 법정동을 찾아서 행정동코드 넘겨주는 함수
+    @Query("SELECT ld.administrativeCode FROM LegalDistrict ld WHERE ld.legalDistrictCode = :legalDistrictCode")
+    Optional<String> findAdministrativeCodeByLegalDistrictCode(@Param("legalDistrictCode") String legalDistrictCode);
+
 }
