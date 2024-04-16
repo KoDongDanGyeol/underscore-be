84de08311d3897cda25fb6d3ef760c85d5166f1e
jjw0071 <jjw0071@naver.com>
2024-04-16 22:54:03 +0900

    feat : 개업 매력도 DTO 생성
    Issues : #29

---
+++ src/main/java/com/kodong/underscore/map/data/BusinessAttractionDTO.java
@@ -0,0 +1,17 @@
+package com.kodong.underscore.map.data;
+
+import lombok.Builder;
+import lombok.Getter;
+
+/**
+ * 특정 행정동의 개업매력도 점수를 넘겨 줍니다.
+ */
+@Getter
+@Builder
+public class BusinessAttractionDTO {
+    private String legalDistrictCode; // 법정동
+    private String administrativeDistrictName; //행정동 이름
+    private int[] businessAttractionScores; // 개업매력도 분야별 점수
+    private int totalScore; //총 점
+
+}
