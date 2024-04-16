c8899bfa02330994f988c7a1e6eec778aef7eb50
Jung Je Won <87056507+jjw0071@users.noreply.github.com>
2024-04-16 22:58:37 +0900

    Feature : 개업 매력도 요청 처리하는 API 추가 (#32)
    * feat : 개업 매력도의 점수를 구성하는 요소들 Enum으로 생성
    
    Issues : #29
    
    * feat : 개업 매력도 계산 및 배열로 전환 시 BusinessAttractionLabels를 참고해서 진행하도록 생성
    
    Issues : #29
    
    * feat : 개업 매력도 DTO 생성
    
    Issues : #29
    
    * feat : 개업 매력도 요청의 Body 매핑 되는 클래스
    
    Issues : #29
    
    * feat : 개업 매력도 요청을 받아 처리 후 결과 넘겨주는 DTO
    
    Issues : #29
    
    * feat : 법정동 코드를 찾아 행정동 코드 넘겨주는 함수 추가
    
    Issues : #29
    
    * feat : controller에 개업 매력도 요청을 처리하는 함수 추가
    
    Issues : #29
    
    * feat : service에 개업 매력도 요청을 처리하는 함수 추가
    
    Issues : #29

--- src/main/java/com/kodong/underscore/map/controller/ScoreApiController.java
+++ src/main/java/com/kodong/underscore/map/controller/ScoreApiController.java
@@ -1,13 +1,21 @@
 package com.kodong.underscore.map.controller;
 
+import com.kodong.underscore.auth.service.CustomOAuth2UserService;
+import com.kodong.underscore.map.data.BusinessAttractionDTO;
+import com.kodong.underscore.map.data.BusinessAttractionRequestData;
+import com.kodong.underscore.map.data.BusinessAttractionResponseDTO;
+import com.kodong.underscore.map.entity.AdministrativeDistrict;
+import com.kodong.underscore.map.entity.ServiceIndustry;
+import com.kodong.underscore.map.repository.AdministrativeDistrictRepository;
+import com.kodong.underscore.map.repository.ServiceIndustryRepository;
 import com.kodong.underscore.map.service.ScoreApiService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,6 +23,9 @@ import java.util.Map;
 public class ScoreApiController {
 
     private final ScoreApiService scoreApiService;
+    private final ServiceIndustryRepository serviceIndustryRepository;
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final AdministrativeDistrictRepository administrativeDistrictRepository;
 
     @GetMapping("serviceIndustryData")
     public ResponseEntity<Map<String,String>> allServiceIndustryInfo(){
@@ -22,6 +33,23 @@ public class ScoreApiController {
         scoreApiService.updateThresholds();
 
         return ResponseEntity.ok().body(dtos);
+    }
+
+    @PostMapping("business-attraction")
+    public ResponseEntity<BusinessAttractionResponseDTO> businessAttractions(@RequestBody BusinessAttractionRequestData requestData){
+        //TODO 로그인 및 결제 처리 체크 하는 로직
+        // boolean loggedIn = customOAuth2UserService.getCurrentUser() != null;
+
+//        if(!loggedIn) {
+//            BusinessAttractionResponseDTO dto = scoreApiService.getBusinessAttractionsForGuestUser(requestData);
+//            return ResponseEntity.ok().body(dto);
+//        }
+
+        BusinessAttractionResponseDTO dto = scoreApiService.getBusinessAttractionsForLoggedInUser(requestData);
+
+        return ResponseEntity.ok().body(dto);
+
+
     }
 
 }
