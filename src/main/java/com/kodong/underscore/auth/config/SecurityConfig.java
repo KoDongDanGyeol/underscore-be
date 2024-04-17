46dc8f0afa964633761c90e570deb813a6fd6ae3
HyunJin Ko <kozzing@daum.net>
2024-04-17 12:29:18 +0900

    Update: SecurityConfig.java

--- src/main/java/com/kodong/underscore/auth/config/SecurityConfig.java
+++ src/main/java/com/kodong/underscore/auth/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
         //oauth2
         http
                 .oauth2Login((oauth2) -> oauth2
-                        .loginPage("http://localhost:3000/auth/join")
+                        .loginPage("/auth/join")
                         .userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig
                                 .userService(customOAuth2UserService)))
                         .successHandler(customSuccessHandler)
