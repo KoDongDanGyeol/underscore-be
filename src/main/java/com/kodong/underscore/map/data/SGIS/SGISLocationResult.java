9629522b1fce18506bad27947923b97bb77d24a2
jjw0071 <jjw0071@naver.com>
2024-04-18 18:12:49 +0900

    feat : SGIS 요청 결과 mapping 하는 클래스들 추가
    Issues :

---
+++ src/main/java/com/kodong/underscore/map/data/SGIS/SGISLocationResult.java
@@ -0,0 +1,24 @@
+package com.kodong.underscore.map.data.SGIS;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SGISLocationResult {
+    @JsonProperty("totalcount")
+    private String totalCount;
+
+    @JsonProperty("resultdata")
+    private List<AddressToLocationDTO> resultData;
+
+    @JsonProperty("matching")
+    private String matching;
+
+    @JsonProperty("pagenum")
+    private String pagenum;
+
+    @JsonProperty("returncount")
+    private String returnCount;
+}
