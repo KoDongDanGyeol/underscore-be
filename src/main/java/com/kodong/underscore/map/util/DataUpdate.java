b3738622e06fe029a2f7bd69b21e063e399b41c1
jjw0071 <jjw0071@naver.com>
2024-04-18 18:16:14 +0900

    feat : csv에서 오류 발생시 처리하도록 하는 로직 추가
    Issues :

--- src/main/java/com/kodong/underscore/map/util/DataUpdate.java
+++ src/main/java/com/kodong/underscore/map/util/DataUpdate.java
@@ -1,6 +1,5 @@
 package com.kodong.underscore.map.util;
 
-import lombok.Data;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -27,6 +26,37 @@ public class DataUpdate {
 
     public HashSet<String> getAdministrativeData() {
         return  administrativeData;
+    }
+
+    // 행정복지 센터 주소를 api로 받았을 때 오류가 생기는 행정구역분류들이 모여있는곳
+    private static final HashSet<String> administrativeClassificationToLocation = new HashSet<>();
+    static{
+        administrativeClassificationToLocation.add("23010660");
+        administrativeClassificationToLocation.add("11160640");
+        administrativeClassificationToLocation.add("11160720");
+        administrativeClassificationToLocation.add("23080530");
+        administrativeClassificationToLocation.add("23080540");
+        administrativeClassificationToLocation.add("24040520");
+        administrativeClassificationToLocation.add("26010540");
+    }
+
+    // 오류가 있는 행정구역분류들 업데이트 대상이 모여 있는 곳
+    private static final HashMap<String, String> administrativeClassificationUpdateData =  new HashMap<>();
+    static {
+        administrativeClassificationUpdateData.put("23010660", "23010681");
+        administrativeClassificationUpdateData.put("11160640", "11160751");
+        administrativeClassificationUpdateData.put("11160720", "11160761");
+        administrativeClassificationUpdateData.put("23080530", "23080531");
+        administrativeClassificationUpdateData.put("23080540", "23080541");
+        administrativeClassificationUpdateData.put("26010540", "26010681");
+    }
+
+    public HashSet<String> getAdministrativeClassificationToLocation() {
+        return  administrativeClassificationToLocation;
+    }
+
+    public HashMap<String, String> getAdministrativeClassificationUpdateData() {
+        return  administrativeClassificationUpdateData;
     }
 
 }
