8877f4a2e8c138704ac0df8202d868101e98343e
minseon48 <97889824+minseon48@users.noreply.github.com>
2024-07-12 00:00:34 +0900

    Feature : 토스페이먼츠 결제 승인 API (#64)
    * membership 저장 entity 생성
    
    * MembershipRepository 생성
    
    * 토스페이먼츠 승인 요청시 필요한 dto 생성
    
    * 결제 승인 사용자에게 보여질 정보 dto 생성
    
    * payment 레포 생성
    
    * 결제 controller 생성
    
    * 토스페이먼츠 결제 승인 요청 service
    
    * 토스페이먼츠 결제 승인 요청 응답 받을 dto 생성
    
    * 결제 entity 생성
    
    * 결제 시 membership 추가, 환불 조회 최대 횟수 설정

--- src/main/java/com/kodong/underscore/auth/entity/User.java
+++ src/main/java/com/kodong/underscore/auth/entity/User.java
@@ -1,6 +1,8 @@
 package com.kodong.underscore.auth.entity;
 
 import com.kodong.underscore.map.entity.AdministrativeDistrict;
+import com.kodong.underscore.pgpayment.entity.Membership;
+import jakarta.annotation.Nullable;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -23,6 +25,12 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<MyPlace> myPlaces = new ArrayList<>();
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "membership_id")
+    private Membership membership;
+    @Nullable
+    private Integer maxRefundableViews;
+
     public User(String username, String name, String email, String role) {
         this.username = username;
         this.name = name;
@@ -38,5 +46,14 @@ public class User {
     public void addMyPlace(MyPlace myPlace) {
         myPlaces.add(myPlace);
         myPlace.setUser(this);
+    }
+
+    public void updateMaxRefundableViews(int maxRefundableViews){
+        this.maxRefundableViews = maxRefundableViews;
+    }
+
+
+    public void updateMembership(Membership membership){
+        this.membership = membership;
     }
 }
