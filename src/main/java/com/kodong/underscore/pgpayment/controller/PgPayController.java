a35a6b11c030a5fec77875d0f68fb2c2afca52b5
minseon48 <97889824+minseon48@users.noreply.github.com>
2024-07-12 14:57:49 +0900

    Revert "Feature : 결제내역 조회 API 추가 (#69)"
    This reverts commit 63b87c0c618b3a40f8a22fff8ccacc5fc2fce1a6.

--- src/main/java/com/kodong/underscore/pgpayment/controller/PgPayController.java
+++ src/main/java/com/kodong/underscore/pgpayment/controller/PgPayController.java
@@ -3,7 +3,7 @@ package com.kodong.underscore.pgpayment.controller;
 import com.kodong.underscore.auth.entity.User;
 import com.kodong.underscore.auth.repository.UserRepository;
 import com.kodong.underscore.pgpayment.dto.*;
-
+import com.kodong.underscore.pgpayment.entity.TossPayment;
 import com.kodong.underscore.pgpayment.repository.PaymentsRepository;
 import com.kodong.underscore.pgpayment.service.PgPayService;
 import lombok.extern.slf4j.Slf4j;
@@ -12,18 +12,22 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/admin/pgPay")
 @Slf4j
 public class PgPayController {
     private final PgPayService pgPayService;
+    private final PaymentsRepository paymentsRepository;
+    private final UserRepository userRepository;
 
-
-    public PgPayController(PgPayService pgPayService) {
+    public PgPayController(PgPayService pgPayService, PaymentsRepository paymentsRepository, UserRepository userRepository) {
         this.pgPayService = pgPayService;
-
+        this.paymentsRepository = paymentsRepository;
+        this.userRepository = userRepository;
     }
 
 
@@ -39,27 +43,13 @@ public class PgPayController {
         log.info("paymentKey={}",paymentKey);
         log.info("amount={}",amount);
 
+        PaymentInfoDto tossPayApproveInfo = pgPayService.approveTossPayments(orderId,paymentKey,amount);
 
-        return pgPayService.approveTossPayments(orderId,paymentKey,amount);
+
+
+        return tossPayApproveInfo;
     }
 
-
-    @GetMapping("{userId}/payment-history")
-    public ResponseEntity<PaymentHistoryList> getPayments(@PathVariable("userId") Long userId,
-                                                          @RequestParam(value = "page", defaultValue = "0") int page,
-                                                          @RequestParam(value="size",defaultValue = "5") int size,
-                                                          @RequestParam(value = "paymentStatus",required = false) String paymentStatus){
-        log.info("userId={}",userId);
-        log.info("paymentStatus={}",paymentStatus);
-
-        Pageable pageable = PageRequest.of(page, size);
-        PaymentHistoryList paymentsHistory = pgPayService.getPaymentsHistory(userId, paymentStatus, pageable);
-
-
-        return ResponseEntity.ok(paymentsHistory);
-
-
-    }
 
 
 
